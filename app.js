c5ddefcbb55efbb95485d24223517b1c80b8c26d
tom lee <tdroid99@gmail.com>
2018-08-03 15:00:17 -0600

    working get basic

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const queries = require('./queries.js')
 
 app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (request,response,next) => {
-    response.send('Hello')
+    queries.allStudents()
+    .then(getAllStudents => {
+        response.json(getAllStudents)
+    })
 })
 
 module.exports = app
